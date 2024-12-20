a5ced574907e912e46ca430e92f20fbbc6793471
Irmak Çağlayan <127547865+irmakcaglayan@users.noreply.github.com>
2024-12-21 00:23:11 +0300

    Basic Calculator Using C++
    This calculator asks the user to enter two numbers (num1 and num2) of their choice.
    Then, it asks the user to choose a function, offering 4 options: Addition (+), Subtraction (-), Multiplication (*), and Division (/).
    If the user's second number (num2) is zero (0) and they choose Division (/), the program outputs: "ERROR: Dividing by zero (0) is not possible!".
    If the user enters an invalid character, the program outputs: "Invalid Choices!".

---
+++ basicCalculatorUsingC++.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int choice;
+    double num1, num2, result;
+    cout << "Enter your first number: "; cin >> num1;
+    cout << "Enter your second number: "; cin >> num2;
+    cout << "Choose the function you want to use (1-4):\n";
+    cout << "1. Addition (+)\n" << "2. Subtraction (-)\n" << "3. Multiplication (*)\n" << "4. Division (/)\n";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        result = num1 + num2;
+        cout << "Result = " << result;
+        break;
+    case 2:
+        result = num1 - num2;
+        cout << "Result = " << result;
+        break;
+    case 3:
+        result = num1 * num2;
+        cout << "Result = " << result;
+        break;
+    case 4:
+        if (num2 != 0){
+            result = num1 / num2;
+            cout << "Result = " << result;}
+        else {
+            cout << "ERROR: Dividing by zero (0) is not possible!";
+        
+           }
+        break;
+    default:
+        cout << "Invalid Choices!";
+        
+    }
+    return 0;
+}
